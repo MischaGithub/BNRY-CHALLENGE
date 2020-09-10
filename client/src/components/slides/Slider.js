23c8aeeba9323b824d6abc2afeb02f1e109af45f
MischaGithub <mischa.achmat120212@gmail.com>
2020-09-10 14:02:48 +0200

    tooltip functionality working stylying to be done

--- client/src/components/slides/Slider.js
+++ client/src/components/slides/Slider.js
@@ -1,24 +1,30 @@
-import React, { Fragment, useRef, useEffect, useContext } from "react";
+import React, { Fragment, useRef, useEffect, useContext , useState} from "react";
 import { Slide } from "react-slideshow-image";
 import ImageContext from "../../context/Images/imageContext";
 import SlideItem from "./SlideItem";
 import Spinner from "../layout/Spinner";
-import ReactTooltip from "react-tooltip";
+import { Button , UncontrolledTooltip } from "reactstrap";
 
-const Slider = ({ slide }) => {
+
+const Slider = ({ slide, $ }) => {
 
      // Initalizing the context
      const imageContext = useContext(ImageContext);
 
      // Pulling out the data with destructing
      const { slides, getSlide, loading } = imageContext;
-
  
      useEffect(() => {
          getSlide();
          // eslint-disable-next-line
      }, []);
+
+
   const slideRef = useRef();
+
+  const [previousIndex, setPreviousIndex] = useState(null);
+  const [nextIndex, setNextIndex ] = useState();
+
 
   const style = {
      textAlign: "center",
@@ -27,7 +33,12 @@ const Slider = ({ slide }) => {
 
   const properties = {
     autoplay: false,
-    arrows: false
+    indicators: false,
+    arrows: false,
+    onChange: (previous, next) => {
+      setPreviousIndex(previous);
+      setNextIndex(next + 2)
+    }
   };
 
   const previous = () => {
@@ -38,41 +49,57 @@ const Slider = ({ slide }) => {
     slideRef.current.goNext();
   }
 
- 
+
   return (
       <Fragment>
           <div > 
         {/*  If slides is not === null and not loading then show slides else show spinner */}
-        {slides !== null && !loading ? (<div>
-        <Slide ref={slideRef} {...properties}>
+        {slides !== null && !loading ? (
+        <Slide ref={slideRef} {...properties} >
             {slides.map(slide =>
-                <div style={style} className="slide">
+                <div key={slideRef} style={style} className="slide">
                     <SlideItem key={slide.id} slide={slide}/>
                 </div>
                 )}
         </Slide>
-      </div>) : <Spinner />}
+      ) : <Spinner />}
       
 
       
         </div>
+
         <div className="autoplay-buttons">
-            <a data-for="soclose" data-tip={slideRef + 1}>?</a>
-            <ReactTooltip id="soclose" 
-                getContent={(dataTip) => 
-                <div><h3>The next slide is {dataTip}</h3></div>}
-                effect="solid"
-                delayHide={500}
-                delayShow={500}
-                delayUpdate={500}
-                place={"right"}
-                border={true}
-                type={"light"}
-                />
-           
-            <button className="btn-sm" type="button" onClick={previous}>Previous</button>
+          <div className="next-button">
+      
             <button className="btn-sm"  type="button" onClick={next}>Next</button>
+    
+            <p ><i className="fa fa-info" style={{textDecoration: "underline", color: "blue"}} href="#" id="UncontrolledTooltipExample" ></i></p>
+            <UncontrolledTooltip style={{ width: "50px", height: "50px"}}placement="right" target="UncontrolledTooltipExample">
+              The next slide is {nextIndex}
+
+            </UncontrolledTooltip>
+   
+
+          </div>
+
+          <div className="previous-button">
+
+
+          <button className="btn-sm" type="button" onClick={previous}>Previous</button>
+
+          <p ><i className="fa fa-info" style={{textDecoration: "underline", color: "blue"}} href="#" id="UncontrolledTooltipExample" ></i></p>
+          <UncontrolledTooltip style={{ width: "50px", height: "50px"}}placement="right" target="UncontrolledTooltipExample">
+            The previous slide is {previousIndex}
+
+          </UncontrolledTooltip>
+   
+
+
+          </div>
+
         </div>
+        
+      
       </Fragment>
     
 
