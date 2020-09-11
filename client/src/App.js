baef85abfef70ebf7b320c08468f64098d984ec4
MischaGithub <mischa.achmat120212@gmail.com>
2020-09-11 12:06:47 +0200

    chnaged files

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from "./components/layout/Navbar";
 import SlideState from "./context/Slides/SlideState";
 import Slider from "./components/Slides/Slider";
-import 'react-slideshow-image/dist/styles.css'
+//import 'react-slideshow-image/dist/styles.css'
 
 const App = () => {
   return (
