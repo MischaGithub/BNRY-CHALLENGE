56f504d71ca6ba502dbfad389d571f29a5185680
MischaGithub <mischa.achmat120212@gmail.com>
2020-09-11 11:21:13 +0200

    Setting up for automated testing

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+
+
+// setUpFliesAfterEnv gives Jest a path(s) for setup to run immediately after the test 
+// framework has been installed in the environment. By default, Jest doesn't know anything
+// about the assertion tomatchImageSnapshot that jest-image-snapshot gives us. So we'll need to extend
+// Jest in setup file to understand how to use this new assertion
+
+// Ref LogRocket frontend Monitoring
+
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+expect.extend({ toMatchImageSnapshot });
+   
